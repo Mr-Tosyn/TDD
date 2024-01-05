7a42b8e646e80f8e2c71500570f6f7a93b469690
Adesua Oluwatosin <51682725+Mr-Tosyn@users.noreply.github.com>
2024-01-05 22:36:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from user_service import user_services
+import requests
 
 app = FastAPI()
+
+products = ['rice','shoes','shirts','pants','socks','hats','gloves','scarves','coats','jackets','sweaters','sweatshirts','shorts','skirts','dresses','suits','blazers','underwear','bras','socks','pajamas','robes','slippers','boots','sandals','sneakers','heels','flats','loafers','oxfords','pumps','t-shirts','polos','tank tops','blouses','tunics','cardigans','turtlenecks','vests','sweatpants','leggings','jeans','trousers','joggers','chinos','overalls','jumpsuits','rompers','swimsuits','cover-ups','sunglasses','eyeglasses','belts','hats','gloves','scarves','umbrellas','watches','bracelets','rings','earrings','necklaces','handbags','backpacks','clutches','briefcases','luggage','totes','wallets','crossbody bags','satchels','duffel bags','tote bags','shoulder bags','hobo bags','bucket bags','messenger bags','wristlets','baguettes','bowling bags','saddle bags','sunglasses','eyeglasses','belts','hats','gloves','scarves','umbrellas','watches','bracelets','rings','earrings','necklaces','handbags','backpacks','clutches','briefcases','luggage','totes','wallets','crossbody bags','satchels','duffel bags','tote bags','shoulder bags','hobo bags','bucket bags','messenger bags','wristlets','baguettes','bowling bags','saddle bags']
 
 @app.get("/")
 async def read_main():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"id": item_id, "name": "Item One", "desscription": "This is item one"}
+@app.get("/items/{item_id}/{item_name}/{item_description}")
+async def read_item(item_id: int, item_name: str, item_description: str):
+    return {"id": item_id, "name": item_name, "desscription": item_description}
+
+
+@app.get("/users")
+def get_users():
+    users = user_services.get_users_from_db()
+    return users
+
+@app.get("/get_activities")
+def get_activities():
+    response = requests.get('https://www.boredapi.com/api/activity')
+    new_json = response.json()
+    return new_json['activity']
