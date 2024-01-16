8f3235e9590f9dad433b6846b7234c9551910312
Adesua Oluwatosin <51682725+Mr-Tosyn@users.noreply.github.com>
2024-01-16 23:58:36 +0100

    Update models.py

--- Database/models.py
+++ Database/models.py
@@ -1,6 +1,7 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
+
 class Todos(Base):
     __tablename__ = 'todos' # This is the name of the table in the database
     
